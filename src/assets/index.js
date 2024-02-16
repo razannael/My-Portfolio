df61dd901921a029732179576b6fd7cb32767d35
razannael <nailrazan40@gmail.com>
2024-02-16 21:43:10 +0200

    my cv

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+// import meta from "./company/meta.png";
+// import shopify from "./company/shopify.png";
+// import starbucks from "./company/starbucks.png";
+// import tesla from "./company/tesla.png";
 import bootstrap from "./tech/bootstrap.png"
 import python from "./tech/python.png"
 import ecommerce from "./ecommerce.png";
@@ -66,10 +66,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   ecommerce,
   todo,
   vtravel,
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,33 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { styles } from '../styles.js'
-
 import {ComputersCanvas} from './canvas'
 function Hero() {
+  const handleFile =()=>{
+    window.open('https://drive.google.com/file/d/14IksOOTn6p2zg_52sQO34o_VGIdAp7-u/view?usp=drive_link')
+  }
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute
       inset-0 top-[90px] max-w-7xl mx-auto flex flex-row
       items-start gap-5`}>
         <div className='flex flex-col justify-center
-        items-center mt-5'>
+        items-center mt-5' >
            <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
           <div className='w-1 sm:h-80 h-40 violet-gradient'/>
         </div>
-        <div>
+        <div className='z-50' >
           <h1 className={`${styles.heroHeadText}
           text-white`}>Hi, I'm <span className='text-[#915eff]'>Razan</span></h1>
           <p className={`${styles.heroSubText} mt-0 text-white-100 mb-5`}>
           I develop user interfaces, and web applications
           </p>
-          <a  href='' download className='cursor-pointer'>
-            <button className='border-2 mt-5 w-44 border-purple-600 rounded-lg px-3 py-2
-           text-purple-400 hover:bg-[#7a50d4]  hover:text-white-100 '> DownLoad My CV</button>
-        </a>
+          <button onClick={handleFile}  className='z-50 border-2 mt-5 w-44 border-[#5c3da0] rounded-lg px-3 py-2
+           text-purple-400 hover:bg-[#5c3da0]  hover:text-white-100 '>Download My CV</button>
         </div>
+        
       </div>
+     
       <ComputersCanvas/>
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
