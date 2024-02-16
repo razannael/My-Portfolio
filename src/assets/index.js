b83b37753900e5ed09dd9464d7999d4c8a9236a4
razannael <nailrazan40@gmail.com>
2024-02-17 00:05:58 +0200

    my changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,6 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-// import meta from "./company/meta.png";
-// import shopify from "./company/shopify.png";
-// import starbucks from "./company/starbucks.png";
-// import tesla from "./company/tesla.png";
 import bootstrap from "./tech/bootstrap.png"
 import python from "./tech/python.png"
 import ecommerce from "./ecommerce.png";
