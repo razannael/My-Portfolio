077ff7f30978e4edbd2d84b9bda16f2e278b5bd1
razannael <nailrazan40@gmail.com>
2024-04-08 00:23:10 +0300

    my changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ function Hero() {
         
       </div>
      
-      {/* <ComputersCanvas/> */}
+      <ComputersCanvas/>
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[25px] h-[47px] rounded-3xl border-4 border-secondary flex justify-center items-start p-1'>
