06077a34060cd18c3b9e70a8e5442915b0331b07
razannael <nailrazan40@gmail.com>
2024-04-07 23:54:35 +0300

    Projects

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -8,10 +8,10 @@ const StarWrapper = (Component, idName) =>
     return (
       <motion.section
         variants={staggerContainer()}
-        initial='hidden'
+        // initial='hidden'
         whileInView='show'
         viewport={{ once: true, amount: 0.25 }}
-        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+        className={`${styles.padding} max-w-6xl mx-auto relative z-0`}
       >
         <span className='hash-span' id={idName}>
           &nbsp;
