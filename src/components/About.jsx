1cb6da333cc6d2d15a09f1780bd8f2dfd0a23929
razannael <nailrazan40@gmail.com>
2024-04-08 13:55:32 +0300

    Add Project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,7 +44,7 @@ function About() {
     <motion.p variants={fadeIn("","",0.1,1)}
     className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
       I'm a skilled software engineer with experience in JavaScript,
-       and expertise in frameworks like React, Node js.
+       and expertise in frameworks like Next.js, React Native, React, Node js.
        I'm a quick learner and collaborate closely with clients to create effeicient,
        scalable, and user-friendly solutions that solve real-world problems.
        Let's work together to bring your ideas to life!
